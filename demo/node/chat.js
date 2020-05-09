d338df02e79a940ae6b84516e5ca8e8ed706d7be
theawless <theawless@gmail.com>
2020-05-09 21:56:48 +0530

    Update the demo to be in sync with the main library

--- demo/node/chat.js
+++ demo/node/chat.js
@@ -1,38 +1,88 @@
+const {DecentSignalChannel, DecentSignalMessage, DecentSignalUser} = require("decent-signal");
 const fs = require("fs");
-const ds = require("decent-signal");
 
-class FileChat extends ds.DecentSignalChannel {
+/**
+ * Hacky implementation for a chat using a local file.
+ * We are using some sync functions because watchers + read/write doesn't work well together.
+ */
+class FileChat extends DecentSignalChannel {
+    /**
+     * @param user {DecentSignalUser}
+     * @param path {string}
+     */
     constructor(user, path) {
         super();
-        this.user = user;
-        this.path = path;
+        this._user = user;
+        this._path = path;
+        this._onFileChanged = (_, __) => this._handleFileChange().then();
     }
 
-    async create() {
-        this.file = await fs.promises.open(this.path, "a+");
-        this.lines = (await this.file.readFile()).toString().split("\n");
-        fs.watchFile(this.path, (_, __) => {
-            const oldLines = this.lines
-            this.file.readFile().then(buffer => {
-                this.lines = dbuffer.toString().split("\n");
-                for (const line of this.lines) {
-                    if (line && oldLines.indexOf(line) === -1) {
-                        const [id, message] = line.split(" :###: ");
-                        const user = new ds.DecentSignalUser(id, undefined);
-                        this.emitter.emit("message-received", user, message);
-                    }
-                }
-            });
-        });
+    /**
+     * Create the chat file.
+     * @returns {Promise<void>}
+     */
+    async createChannel() {
+        const file = await fs.promises.open(this._path, "a+");
+        await file.close();
     }
 
-    async close() {
-        await this.file.close();
-        await fs.promises.unlink(this.path);
+    /**
+     * Join the channel by listening to the file events.
+     * @returns {Promise<void>}
+     */
+    async joinChannel() {
+        this.lines = fs.readFileSync(this._path).toString().split("\n");
+        fs.watchFile(this._path, this._onFileChanged);
     }
 
+    /**
+     * Stop listening to updates in the file.
+     * @returns {Promise<void>}
+     */
+    async leaveChannel() {
+        fs.unwatchFile(this._path, this._onFileChanged);
+    }
+
+    /**
+     * Delete the chat file.
+     * @returns {Promise<void>}
+     */
+    async deleteChannel() {
+        await fs.promises.unlink(this._path);
+    }
+
+    /**
+     * Handle watch file event and send message-received signal for new messages.
+     */
+    async _handleFileChange() {
+        const oldLines = this.lines;
+        this.lines = fs.readFileSync(this._path).toString().split("\n");
+        for (const line of this.lines) {
+            if (line !== undefined && oldLines.indexOf(line) === -1) {
+                const entry = JSON.parse(line);
+                const from = new DecentSignalUser(entry.from, undefined);
+                const to = entry.to === undefined ? undefined : new DecentSignalUser(entry.to, undefined);
+                const message = new DecentSignalMessage(entry.party, to, entry.type, entry.text);
+                this.events.emit("message-received", from, message);
+            }
+        }
+    }
+
+    /**
+     * Send message to the channel.
+     * @param message {DecentSignalMessage}
+     * @returns {Promise<void>}
+     */
     async sendMessage(message) {
-        await this.file.write(this.user.id + " :###: " + message + "\n");
+        const line = JSON.stringify({
+            from: this._user.id,
+            to: message.to === undefined ? undefined : message.to.id,
+            party: message.party,
+            type: message.type,
+            text: message.message
+        });
+        this.lines.push(line);
+        fs.writeFileSync(this._path, this.lines.join("\n"));
     }
 }
 
--- demo/node/index.js
+++ demo/node/index.js
@@ -1,40 +1,96 @@
-const ds = require("decent-signal");
-const dsc = require("decent-signal-crypto");
-const wrtc = require('wrtc');
+const {DecentSignal, DecentSignalNode, DecentSignalUser} = require("decent-signal");
+const {DecentSignalCrypto} = require("decent-signal-crypto");
+const {FileChat} = require("./chat");
+const wrtc = require("wrtc");
 const Peer = require("simple-peer");
-const c = require("./chat");
 
-async function main() {
-    const user = new ds.DecentSignalUser(process.argv[2], undefined);
-    const chat = new c.FileChat(user, "channel.txt");
-    const crypto = new dsc.DecentSignalCrypto();
-    const signal = new ds.DecentSignal(user, chat, crypto, {
-        party: process.argv[3],
-        password: process.argv[4]
-    });
-    await chat.create();
-    await signal.startSignalling();
-
-    const peers = new Map();
-    signal.emitter.connect("node-discovered", (which) => {
-        const peer = new Peer({wrtc: wrtc, initiator: user.id < which.id});
-        peer.on("signal", (data) => {
-            const stuff = JSON.stringify(data);
-            signal.sendSignallingData(which, stuff);
+/**
+ * Demo application for node.
+ * Run the scripts from package.json to see them perform signalling and then hi! to each other.
+ */
+class HelloWorld {
+    /**
+     * Read the inputs from program arguments.
+     */
+    constructor() {
+        this._crypto = new DecentSignalCrypto();
+        this._user = new DecentSignalUser(process.argv[2], undefined);
+        this._chat = new FileChat(this._user, "channel.txt");
+        this._signal = new DecentSignal(this._user, this._chat, this._crypto, {
+            party: process.argv[3],
+            password: process.argv[4]
         });
-        peer.on('connect', () => {
-            peer.send(`Hello from ${user.id}!`);
-        })
-        peer.on('data', data => {
-            console.log(`Got a message: ${data}`)
-        })
-        peers.set(which.id, peer);
-    });
-    signal.emitter.connect("signal-received", (from, stuff) => {
-        const peer = peers.get(from.id);
-        const data = JSON.parse(stuff);
-        peer.signal(data);
-    });
+        this._peers = new Map(); // map of user id to peer
+        this._onNodeDiscovery = (node) => this._handleDiscovery(node).then();
+        this._onSignalReceived = (node, data) => this._handleSignal(node, data).then();
+    }
+
+    /**
+     * Start the demo.
+     * @returns {Promise<void>}
+     */
+    async start() {
+        if (this._user.id === "0") {
+            await this._chat.createChannel();
+        }
+        await this._chat.joinChannel();
+        await this._signal.startSignalling();
+        this._signal.events.connect("node-discovered", this._onNodeDiscovery);
+        this._signal.events.connect("signal-received", this._onSignalReceived);
+    }
+
+    /**
+     * Stop the demo.
+     * @returns {Promise<void>}
+     */
+    async stop() {
+        this._signal.events.disconnect("node-discovered", this._onNodeDiscovery);
+        this._signal.events.disconnect("signal-received", this._onSignalReceived);
+        await this._signal.stopSignalling();
+        await this._chat.leaveChannel();
+        if (this._user.id === "0") {
+            await this._chat.deleteChannel();
+        }
+    }
+
+    /**
+     * Create a new peer connection for each node.
+     * @param node {DecentSignalNode}
+     * @returns {Promise<void>}
+     */
+    async _handleDiscovery(node) {
+        const peer = new Peer({wrtc: wrtc, initiator: this._user.id < node.user.id});
+        this._peers.set(node.user.id, peer);
+        peer.on("signal", (data) => {
+            this._signal.sendSignal(node, JSON.stringify(data)).then();
+        });
+        peer.on("connect", () => {
+            peer.send(`Hello from ${this._user.id}!`);
+        });
+        peer.on("data", data => {
+            console.log(`Got a message: ${data}`);
+        });
+    }
+
+    /**
+     * @param node {DecentSignalNode}
+     * @param data {string}
+     * @returns {Promise<void>}
+     */
+    async _handleSignal(node, data) {
+        const peer = this._peers.get(node.user.id);
+        peer.signal(JSON.parse(data));
+    }
+}
+
+/**
+ * Async main function.
+ * @returns {Promise<void>}
+ */
+async function main() {
+    const demo = new HelloWorld();
+    await demo.start();
+    process.on("SIGINT", () => demo.stop().then(() => process.exit()));
 }
 
 main().then();
