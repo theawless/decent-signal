54b8e9f50e968c5dc4a7b0d973c7b12ebebb581a
theawless <theawless@gmail.com>
2020-09-26 21:05:56 +0530

    Re-write the bundler example with local server

--- examples/bundler/README.md
+++ examples/bundler/README.md
@@ -1,17 +1,14 @@
 # decent-signal-example-bundler
 
-Example for bundler + manual webrtc + subtle crypto + local chat + rank based network.
+Example for bundler + subtle crypto + public key communication + local server + manual webrtc signalling.
 
 ## Demo
 
 * Serve with `npx serve` in the root directory
-* Open [node-0011](http://localhost:5000/examples/bundler/?id=0&rank=0&party=party1&pass=pass1)
-* Open [node-1111](http://localhost:5000/examples/bundler/?id=1&rank=1&party=party1&pass=pass1)
-* Open [node-2112](http://localhost:5000/examples/bundler/?id=2&rank=1&party=party1&pass=pass2)
-* Open [node-3111](http://localhost:5000/examples/bundler/?id=3&rank=1&party=party1&pass=pass1)
-* Open [node-0122](http://localhost:5000/examples/bundler/?id=0&rank=1&party=party2&pass=pass2)
-* Open [node-1022](http://localhost:5000/examples/bundler/?id=1&rank=0&party=party2&pass=pass2)
-* Open [node-2021](http://localhost:5000/examples/bundler/?id=2&rank=0&party=party2&pass=pass1)
+* Open [node-0](http://localhost:5000/examples/bundler/?id=0)
+* Open [node-1](http://localhost:5000/examples/bundler/?id=1)
+* Open [node-2](http://localhost:5000/examples/bundler/?id=2)
+* Open [node-3](http://localhost:5000/examples/bundler/?id=3)
 
 ## Documentation
 
--- examples/bundler/index.js
+++ examples/bundler/index.js
@@ -1,164 +1,225 @@
-import { DecentSignal } from 'decent-signal'
+import { DecentSignal, DecentSignalMessage, DecentSignalPublicKeyCommunicator, DecentSignalUser } from 'decent-signal'
+import { DecentSignalLocalServer } from 'decent-signal-adapter-local-server'
 import { DecentSignalSubtleCrypto } from 'decent-signal-adapter-subtle-crypto'
-import { DecentSignalLocalChat, DecentSignalLocalChatUser } from 'decent-signal-adapter-local-chat'
 
 /**
- * Example for bundler + manual webrtc + subtle crypto + local chat + rank based network.
- * See that 1. one channel can have multiple parties 2. nodes with wrong pass cannot join.
- * Serve and open the urls nodes in README to see them perform signalling and then say hi! to each other.
- * Each user rejects to do handshake with a user that is on same priority. If x1 > x2 then x2 is the initiator.
+ * Example for bundler + subtle crypto + public key communication + local server + manual webrtc signalling.
+ * TODO: Can use perfect negotiation here.
  */
-class HelloWorld {
+class Demo {
   /**
    * @param {RxDatabaseBase} db
    * @param {string} id
-   * @param {number} rank
-   * @param {string} party
-   * @param {string} pass
    */
-  constructor (db, { id, rank, party, pass }) {
-    this._crypto = new DecentSignalSubtleCrypto()
-    this._user = new DecentSignalLocalChatUser(id, rank)
-    this._chat = new DecentSignalLocalChat(db, this._user)
-    this._signal = new DecentSignal(this._user, this._chat, this._crypto, { party, pass })
-    this._peers = new Map() // map of user id to peer
-    this._onUserSeen = (user, accept) => this._handleSeen(user, accept).then()
-    this._onNodeDiscovery = (node) => this._handleDiscovery(node).then()
-    this._onSignalReceived = (node, data) => this._handleSignal(node, data).then()
+  constructor (db, { id }) {
+    this._user = new DecentSignalUser(id)
+    this._server = new DecentSignalLocalServer(db, this._user)
+    const crypto = new DecentSignalSubtleCrypto()
+    const communicator = new DecentSignalPublicKeyCommunicator(crypto)
+    this._signal = new DecentSignal(communicator, this._server)
+    this._peers = new Map() // user id to peer map
+    this._feeds = new Map() // user id to feed map
+    this._onUserSeen = (user) => this._handleUser(user, true).then()
+    this._onUserLeft = (user) => this._handleUser(user, false).then()
+    this._onServerMessageReceived = (from, message) => this._handleServerMessage(from, message)
+    this._onMessageReceived = (from, message) => this._handleMessage(from, message).then()
   }
 
   /**
    * Start the demo.
-   * @returns {Promise<void>}
    */
   async start () {
-    await this._chat.startListening()
     await this._signal.startSignalling()
     this._signal.events.connect('user-seen', this._onUserSeen)
-    this._signal.events.connect('node-discovered', this._onNodeDiscovery)
-    this._signal.events.connect('signal-received', this._onSignalReceived)
+    this._signal.events.connect('user-left', this._onUserLeft)
+    this._signal.events.connect('message-received', this._onMessageReceived)
+    this._server.events.connect('message-received', this._onServerMessageReceived)
+    for (const user of await this._signal.getUsers()) {
+      await this._handleUser(user, true)
+    }
+    this._setupUI()
   }
 
   /**
    * Stop the demo.
-   * @returns {Promise<void>}
    */
   async stop () {
-    this._signal.events.disconnect('signal-received', this._onSignalReceived)
-    this._signal.events.disconnect('node-discovered', this._onNodeDiscovery)
+    for (const peer of this._peers.values()) {
+      peer.close()
+    }
+    this._server.events.disconnect('message-received', this._onServerMessageReceived)
+    this._signal.events.disconnect('message-received', this._onMessageReceived)
     this._signal.events.disconnect('user-seen', this._onUserSeen)
+    this._signal.events.disconnect('user-left', this._onUserLeft)
     await this._signal.stopSignalling()
-    await this._chat.stopListening()
   }
 
   /**
-   * Decide whether we want to do handshake with the user.
-   * @param {DecentSignalLocalChatUser} user
-   * @param {function():void} accept
-   * @returns {Promise<void>}
+   * Handle user updates by starting webrtc connections.
+   * @param {DecentSignalUser} user
+   * @param {boolean} active
    */
-  async _handleSeen (user, accept) {
-    if (this._user.rank - user.rank === 0) {
-      console.info(`Skipping handshake for user ${user.id}.`)
+  async _handleUser (user, active) {
+    if (!active) {
+      this._peers.get(user.id).close()
+      this._peers.delete(user.id)
+      this._feeds.delete(user.id)
+      this._updateServerPeers()
       return
     }
-    accept()
-  }
-
-  /**
-   * Create a new peer connection for each node.
-   * @param {DecentSignalNode} node
-   * @returns {Promise<void>}
-   */
-  async _handleDiscovery (node) {
+    await this._signal.connectUser(user)
     const peer = new window.RTCPeerConnection()
-    this._peers.set(node.user.id, peer)
-    peer.onicecandidate = (event) => {
-      if (event.candidate) {
-        this._signal.sendSignal(node, JSON.stringify({ ice: event.candidate })).then()
+    this._peers.set(user.id, peer)
+    this._updateServerPeers()
+    peer.addEventListener('icecandidate', async (event) => {
+      if (event.candidate && peer === this._peers.get(user.id)) {
+        const message = new DecentSignalMessage(JSON.stringify({ ice: event.candidate }))
+        await this._signal.sendMessage(user, message)
       }
-    }
-    if (this._user.rank - node.user.rank < 0) { // initiator
-      peer.onnegotiationneeded = () => {
-        peer.createOffer().then((desc) => this._setupDescription(node, peer, desc).then())
+    })
+    const initiator = this._user.id > user.id
+    peer.addEventListener('connectionstatechange', () => {
+      this._updateWebrtcPeers()
+      if (initiator && peer.connectionState === 'failed' && peer === this._peers.get(user.id)) {
+        console.log(`Re-initiating connection with user ${user.id}`)
+        peer.restartIce()
       }
-      this._setupChat(peer.createDataChannel('chat'))
+    })
+    if (initiator) {
+      console.log(`Initiating connection with user ${user.id}`)
+      peer.addEventListener('negotiationneeded', async () => {
+        const offer = await peer.createOffer()
+        await peer.setLocalDescription(offer)
+        const message = new DecentSignalMessage(JSON.stringify({ offer: peer.localDescription }))
+        // wait for others to be ready for receiving our signals
+        await new Promise(resolve => setTimeout(resolve, 4000))
+        await this._signal.sendMessage(user, message)
+      })
+      const feed = peer.createDataChannel('feed')
+      this._feeds.set(user.id, feed)
+      feed.addEventListener('message', (event) => {
+        this._handleWebrtcMessage(user, event.data)
+      })
     } else {
-      peer.ondatachannel = (event) => {
-        this._setupChat(event.channel)
-      }
+      console.log(`Receiving connection from user ${user.id}`)
+      peer.addEventListener('datachannel', (event) => {
+        this._feeds.set(user.id, event.channel)
+        event.channel.addEventListener('message', (event) => {
+          this._handleWebrtcMessage(user, event.data)
+        })
+      })
     }
   }
 
   /**
-   * Setup local description and send it.
-   * @param {DecentSignalNode} node
-   * @param {RTCPeerConnection} peer
-   * @param {RTCSessionDescription | RTCSessionDescriptionInit} desc
+   * Setup the ui functionality.
    */
-  async _setupDescription (node, peer, desc) {
-    await peer.setLocalDescription(desc)
-    await this._signal.sendSignal(node, JSON.stringify({ sdp: peer.localDescription }))
+  _setupUI () {
+    const serverInput = document.getElementById('server-input')
+    serverInput.addEventListener('keyup', (event) => {
+      event.preventDefault()
+      if (event.key === 'Enter') {
+        const text = serverInput.value.trim()
+        serverInput.value = ''
+        if (text) {
+          const message = new DecentSignalMessage(text)
+          this._handleServerMessage(this._user, message)
+          this._server.sendMessage(undefined, message).then()
+        }
+      }
+    })
+    const webRtcInput = document.getElementById('webrtc-input')
+    webRtcInput.addEventListener('keyup', (event) => {
+      event.preventDefault()
+      if (event.key === 'Enter') {
+        const text = webRtcInput.value.trim()
+        webRtcInput.value = ''
+        if (text) {
+          this._handleWebrtcMessage(this._user, text)
+          for (const feed of this._feeds.values()) {
+            feed.send(text)
+          }
+        }
+      }
+    })
   }
 
   /**
-   * Connect to various events of data channel.
-   * @param {RTCDataChannel} chat
+   * Show the server peers on the UI.
    */
-  _setupChat (chat) {
-    chat.onmessage = (event) => console.info(`Got a message: "${event.data}".`)
-    chat.onopen = (_) => chat.send(`Hello from ${this._user.id}!`)
-  };
+  _updateServerPeers () {
+    let text = ''
+    for (const user of this._peers.keys()) {
+      text += `${user}\n`
+    }
+    document.getElementById('server-peers').textContent = text
+  }
 
   /**
-   * Set the remote description or ice candidate when signal arrives from the other node.
-   * @param {DecentSignalNode} node
-   * @param {string} data
-   * @returns {Promise<void>}
+   * Show the webrtc peers on the UI.
    */
-  async _handleSignal (node, data) {
-    const peer = this._peers.get(node.user.id)
-    const message = JSON.parse(data)
-    if (message.sdp) {
-      await peer.setRemoteDescription(new window.RTCSessionDescription(message.sdp))
-      if (peer.remoteDescription.type === 'offer') {
-        const desc = await peer.createAnswer()
-        await this._setupDescription(node, peer, desc)
-      }
-    } else if (message.ice) {
-      await peer.addIceCandidate(new window.RTCIceCandidate(message.ice))
-    } else {
-      console.info(`Received weird signalling data from user ${node.user.id}.`)
+  _updateWebrtcPeers () {
+    let text = ''
+    for (const [user, peer] of this._peers) {
+      text += `${user}: ${peer.connectionState}\n`
+    }
+    document.getElementById('webrtc-peers').textContent = text
+  }
+
+  /**
+   * Show the server messages on the UI.
+   * @param {DecentSignalUser} from
+   * @param {DecentSignalMessage} message
+   */
+  _handleServerMessage (from, message) {
+    document.getElementById('server-messages').textContent += `${from.id}: ${message.text}\n`
+  }
+
+  /**
+   * Show the webrtc messages on the UI.
+   * @param {DecentSignalUser} from
+   * @param {string} message
+   */
+  _handleWebrtcMessage (from, message) {
+    document.getElementById('webrtc-messages').textContent += `${from.id}: ${message}\n`
+  }
+
+  /**
+   * Handle signalling data when it arrives from the other user.
+   * @param {DecentSignalUser} from
+   * @param {DecentSignalMessage} message
+   */
+  async _handleMessage (from, message) {
+    const peer = this._peers.get(from.id)
+    const signal = JSON.parse(message.text)
+    if (signal.answer) {
+      await peer.setRemoteDescription(new window.RTCSessionDescription(signal.answer))
+    } else if (signal.offer) {
+      await peer.setRemoteDescription(new window.RTCSessionDescription(signal.offer))
+      const answer = await peer.createAnswer()
+      await peer.setLocalDescription(answer)
+      const message = new DecentSignalMessage(JSON.stringify({ answer: peer.localDescription }))
+      await this._signal.sendMessage(from, message)
+    } else if (signal.ice) {
+      await peer.addIceCandidate(new window.RTCIceCandidate(signal.ice))
     }
   }
-}
-
-/**
- * Log to the console widget on the page.
- * @param {...*} args
- */
-console.info = function (...args) {
-  const message = args.map(x => typeof x === 'object' ? JSON.stringify(x) : x)
-  document.getElementById('console').textContent += message + '\n'
 }
 
 /**
  * Async main function.
- * @returns {Promise<void>}
  */
 async function main () {
-  const db = await window.RxDB.createRxDatabase({ name: 'channel', adapter: 'idb' })
+  const db = await window.RxDB.createRxDatabase({ name: 'demo', adapter: 'idb' })
   const args = new URLSearchParams(window.location.search)
-  const demo = new HelloWorld(db, {
-    id: args.get('id'),
-    rank: parseInt(args.get('rank')),
-    party: args.get('party'),
-    pass: args.get('pass')
-  })
+  const demo = new Demo(db, { id: args.get('id') })
   await demo.start()
-  window.addEventListener('beforeunload', (_) => {
-    demo.stop().then()
-  })
+  const clean = (event) => {
+    demo.stop().then(() => window.removeEventListener('beforeunload', clean))
+    event.preventDefault()
+    event.returnValue = ''
+  }
+  window.addEventListener('beforeunload', clean)
 }
 
 main().then()
